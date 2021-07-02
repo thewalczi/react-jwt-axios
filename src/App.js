8593e8ccae8ab75da0e13c9cb5bf87a6e7dfbdd7
thewalczi <walczak.kam@gmail.com>
2021-07-02 22:59:00 +0200

    Initialize app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const App = () => {
+	return <div className="App">Hello</div>;
+};
+
+export default App;
